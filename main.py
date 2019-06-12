c13a2e9deacd38b0c526e6a843b5d3237bc372c3
Daniel <ssw1200@gmail.com>
2019-06-12 07:20:38 -0700

    fix bug

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from keras.models import Sequential
-from keras.layers import Dense, MaxPooling2D, Conv2D
+from keras.layers import Dense, MaxPooling2D, Conv2D, Flatten
 from keras.utils import to_categorical
 import numpy as np
 from keras.datasets import mnist
@@ -14,7 +14,7 @@ train_data = train_data.reshape((60000, 28, 28, 1))
 test_data = test_data.reshape((10000, 28, 28, 1))
 
 # Normalize pixel values to be between 0 and 1
-train_images, test_images = train_images / 255.0, test_images / 255.0
+train_data, test_data = train_data / 255.0, test_data / 255.0
 
 # Defined some initial params
 output_nodes_count = 10
@@ -24,7 +24,7 @@ labels, test_labels = to_categorical(train_label), to_categorical(test_label)
 
 # Create model
 model = Sequential()
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)) # the 1st 2d-convolutional layer
+model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1))) # the 1st 2d-convolutional layer
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Conv2D(64, (3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
